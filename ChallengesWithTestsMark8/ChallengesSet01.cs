93741715174304fed44cbae96c337a82f6c929f3
Argee Abraham <rg360@ymail.com>
2020-09-09 18:52:29 -0500

    first commit

---
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet01
+    {
+        public bool AreTwoNumbersTheSame(int num1, int num2)
+        {
+            return num1 == num2;           
+        }
+
+        public double Subtract(double minuend, double subtrahend)
+        {
+            return (minuend - subtrahend);
+        }
+
+        public int Add(int number1, int number2)
+        {
+            return (number1 + number2);
+        }
+
+        public int GetSmallestNumber(int number1, int number2)
+        {
+            //throw new NotImplementedException();
+            if (number1 <= number2)
+            {
+                return number1;
+            } else
+            {
+                return number2;
+            }
+        }
+
+        public long Multiply(long factor1, long factor2)
+        {
+            return (factor1 * factor2);
+        }
+
+        public string GetGreeting(string nameOfPerson)
+        {
+            //throw new NotImplementedException();
+         
+                string x = "Hello, " + nameOfPerson + "!";
+                return x;
+          
+            
+        }
+
+        public string GetHey()
+        {
+            return "HEY!";
+        }
+    }
+}
---
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet03
+    {
+        public bool ArrayContainsAFalse(bool[] vals)
+        {
+            //throw new NotImplementedException();
+
+            bool checkBool = false;
+            int pos = Array.IndexOf(vals, checkBool);
+            if (pos > -1)
+            {
+                return true;
+            }
+                else
+            {
+                return false;
+            }
+
+        }
+
+        public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
+        {
+            //throw new NotImplementedException();
+            int result = 0;
+            
+
+            foreach (int i in numbers)
+                if (i % 2 != 0)
+                {
+                    result += i;
+                }
+
+          
+            return result % 2 != 0;
+        }
+
+        public bool PasswordContainsUpperLowerAndNumber(string password)
+        {
+            //throw new NotImplementedException();
+            bool containsUpper = false;
+            bool containsLower = false;
+            bool containsNumber = false;
+            for (int i = 0; i < password.Length
+                && !(containsUpper && containsLower && containsNumber); i++)
+            {
+                char c = password[i];
+                if (!containsUpper) containsUpper = char.IsUpper(c);
+                if (!containsLower) containsLower = char.IsLower(c);
+                if (!containsNumber) containsNumber = char.IsDigit(c);
+            }
+            return containsUpper && containsLower && containsNumber;
+
+            //for (int i = 0; i < password.Length; i++)
+            {
+
+                //if (char.IsLower(password[i]) && char.IsUpper(password[i])
+                //    && char.IsDigit(password[i]))
+                //{
+                //    return true;
+                //}
+            }
+            //return false;
+        }
+
+        public char GetFirstLetterOfString(string val)
+        {
+            //throw new NotImplementedException();
+            char firstChar = val[0];
+            return firstChar;
+        }
+
+        public char GetLastLetterOfString(string val)
+        {
+            throw new NotImplementedException();
+
+            //char lastChar = val.Last();
+
+            //return lastChar;
+        }
+
+        public decimal Divide(decimal dividend, decimal divisor)
+        {
+            //throw new NotImplementedException();
+
+            return dividend / divisor;
+        }
+
+        public int LastMinusFirst(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] GetOddsBelow100()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeAllElementsToUppercase(string[] words)
+        {
+            //throw new NotImplementedException();
+            //foreach (string wordUp in words)
+            //    wordUp = words.ToUpper();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+
+            }
+
+        }
+    }
+}
